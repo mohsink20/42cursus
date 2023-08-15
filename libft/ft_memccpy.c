01b6392abc7d1efb2bccbd32e72fb75d6999e668
shu <sshuytbusiness@gmail.com>
2023-08-15 18:26:55 +0700

    Update

---
+++ libft/ft_memccpy.c
@@ -0,0 +1,15 @@
+#include "libft.h"
+
+void	*ft_memcpy(void *dest, const void *src, int a, size_t b)
+{
+	size_t	i;
+
+	i = -1;
+	while (++i < b)
+	{
+		((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+		if(((unsigned char *)src)[i] == (unsigned char)a)
+			return (dest + i + 1);
+	}
+	return (NULL);
+}
