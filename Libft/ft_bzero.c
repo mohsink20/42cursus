5d65aa7ac94ecd03ff54320c6a3dfe2fdcc2e719
shu <sshuytbusiness@gmail.com>
2023-08-15 17:41:57 +0700

    Update

--- Libft/ft_bzero.c
+++
@@ -1,6 +0,0 @@
-#include "libft.h"
-
-void	ft_bzero(void *s, size_t a)
-{
-	ft_memset(s, 0, a);
-}
--- Libft/libft.h
+++
@@ -1,9 +0,0 @@
-#ifndef LIBFT_H
-# define LIBFT_H
-
-#include <unistd.h>
-#include <stdio.h>
-
-size_t	ft_strlen(const	char *s);
-
-#endif
