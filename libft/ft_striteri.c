6a125d048bda7a01910ef1c78e24f4a665e55d22
shu <sshuytbusiness@gmail.com>
2023-09-07 16:03:29 +0700

    ft_striteri.c

---
+++ libft/ft_striteri.c
@@ -0,0 +1,18 @@
+#include "libft.h"
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char *))
+{
+	char			*str;
+	unsigned int	counter;
+
+	str = (char *)s;
+	if (str && f)
+	{
+		counter = 0;
+		while (str[counter])
+		{
+			(*f)(counter, &str[counter]);
+			counter++;
+		}
+	}
+}
